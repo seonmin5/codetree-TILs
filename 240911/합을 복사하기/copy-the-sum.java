e0c9700508b5b6e239edf170e11d3c9559b5135b
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-11 00:45:38 +0900

    Title: 합을 복사하기, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240911/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int result = a + b + c;
+        a = result;
+        b = result;
+        c = result;
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
