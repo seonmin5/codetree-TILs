8f27d97dd3858fbbb1c71f5ea69e3628bf5d5ec7
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-14 00:14:09 +0900

    Title: 데이터 교환, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240914/데이터 교환/exchange-data.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int temp = c;
+        c = b;
+        b = a;
+        a = temp;
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
