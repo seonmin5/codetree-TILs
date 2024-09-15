3b483d53d36f63102cd13408a139ad546b7b6711
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-15 20:47:17 +0900

    Title: 세 수의 중간값, Time: 92ms, Memory: 8MB, Status: Passed - CodeTree

--- 240915/세 수의 중간값/median-of-three-numbers.java
+++ 240915/세 수의 중간값/median-of-three-numbers.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int a, b, c;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
 
         if (b > a && b < c) {
             System.out.println(1);
         } else {
             System.out.println(0);
         }
-        sc.close();
+
+        br.close();
     }
 }
