bb430b6b0ebae928a89c156c735517b753165750
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-20 23:45:33 +0900

    Title: 1부터 n까지 출력, Time: 125ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240920/1부터 n까지 출력/print-from-1-to-n.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.printf("%d ", i);
+        }
+
+    }
+}
