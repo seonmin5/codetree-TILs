2ed5c6e48f5fdc4d6b9f07dba81317afa9e5ae48
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-22 00:20:14 +0900

    Title: 홀수만 출력 2, Time: 91ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240922/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static boolean isOdd(int number) {
+        return number % 2 == 1;
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a, b;
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        for (int i = a; i >= b; i--) {
+            if (isOdd(i)) {
+                System.out.printf("%d ", i);
+            }
+        }
+
+    }
+}
