1d9ebb86c9682093662997e0d9176760f80d6c1a
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-09-29 15:53:27 +0900

    Title: 입력받은 수의 합과 평균, Time: 104ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240929/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine().trim());
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            int number = Integer.parseInt(br.readLine().trim());
+            sum += number;
+        }
+
+        double average = (double) sum/n;
+        System.out.printf("%d %.1f", sum, average);
+        br.close();
+    }
+}
