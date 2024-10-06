7ac777ff27b8a8608dab0a9c80a42f24c12f1164
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-06 17:29:08 +0900

    Title: 체크 다이아몬드, Time: 117ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241006/체크 다이아몬드/check-diamond.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static void printPattern(int n) {
+        for (int i = 0; i<n; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        printPattern(n);
+    }
+}
