c8afe8e0a8a460d079a4add9b447566f22a38684
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-13 08:50:11 +0900

    Title: 점수 입출력, Time: 112ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241013/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.printf("Your score is %d point.", n);
+        br.close();
+    }
+}
