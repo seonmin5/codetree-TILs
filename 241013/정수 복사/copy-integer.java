2196120a5936da5f782e7b6ddcf16cf486bbac3e
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-13 08:41:05 +0900

    Title: 정수 복사, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241013/정수 복사/copy-integer.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        int b = 4;
+        b = a;
+        int result = a*b;
+        System.out.printf("%d %d\n", a, b);
+        System.out.println(result);
+    }
+}
