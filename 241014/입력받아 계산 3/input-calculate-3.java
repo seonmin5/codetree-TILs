fbf4c7812ab2bd9906f87b9826f5ead270858c91
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-14 10:44:05 +0900

    Title: 입력받아 계산 3, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241014/입력받아 계산 3/input-calculate-3.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        StringTokenizer st1 = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st1.nextToken());
+        
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(st2.nextToken());
+        
+        System.out.print(a*b);
+        br.close();
+    }
+}
