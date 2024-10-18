82b637a9e1a08db131de0a31d9069644a7589df6
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-19 08:52:08 +0900

    Title: 범위 밖의 수, Time: 95ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241019/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        br.close();
+
+        if (a < 10 || a > 20) {
+            System.out.print("yes");
+        } System.out.print("no");
+    }
+}
