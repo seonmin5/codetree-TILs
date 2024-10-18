8ca623fa69b8cf7556f909c3459af72929c0c8f5
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-18 10:54:41 +0900

    Title: 점수 비교, Time: 116ms, Memory: 8MB, Status: Passed - CodeTree

--- 241018/점수 비교/score-comparison.java
+++ 241018/점수 비교/score-comparison.java
@@ -6,19 +6,13 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st1 = new StringTokenizer(br.readLine());
-        int mathA = Integer.parseInt(st1.nextToken());
-        int engA = Integer.parseInt(st1.nextToken());
-        
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int mathB = Integer.parseInt(st2.nextToken());
-        int engB = Integer.parseInt(st2.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine() + " " + br.readLine());
+        int mathA = Integer.parseInt(st.nextToken());
+        int engA = Integer.parseInt(st.nextToken());
+        int mathB = Integer.parseInt(st.nextToken());
+        int engB = Integer.parseInt(st.nextToken());
         br.close();
 
-        if (mathA > mathB && engA > engB) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
-        }
+        System.out.print((mathA > mathB && engA > engB) ? 1 : 0);
     }
 }
