8d699dc065b89d62f39c0eee416e30520d1973fc
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-19 12:11:53 +0900

    Title: 윤년인가, Time: 102ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241019/윤년인가/is-leap-year.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static boolean isLeapYear(int n) {
+        if (n % 100 == 0 && n % 400 == 0) {
+            return false;
+        } else if (n % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(br.readLine());
+        br.close();
+        System.out.print(isLeapYear(y));
+    }
+}
