b4996b3b6482d2c1d7fdcf39bc10b99ac00cc1aa
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-24 08:54:06 +0900

    Title: 높은 수에서 낮은 수까지, Time: 83ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 241024/높은 수에서 낮은 수까지/from-high-to-low.java
+++ 241024/높은 수에서 낮은 수까지/from-high-to-low.java
@@ -4,16 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    private static void descNumber(int a, int b) {
+        for (int i = b; i >= a; i--) {
+            System.out.printf("%d ", i);
+        }
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine() + " " + br.readLine());
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        
-        for (int i = b; i >= a; i--) {
-            System.out.printf("%d ", i);
-        }
-
         br.close();
+        descNumber(a, b);
     }
 }
