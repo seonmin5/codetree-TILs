bad3853bda76d6fdd67323a5a25a7e3e9e434f14
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-27 12:33:14 +0900

    Title: 100부터 입력받는 수까지의 합, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241027/100부터 입력받는 수까지의 합/sum-of-the-number-input-from-100.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+        for (int i = n; i <= 100; i++) {
+            result += i;
+        }
+        System.out.print(result);
+        br.close();
+    }
+}
