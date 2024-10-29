4748e4e33369d69009245a65380c2bc296c8d0b2
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-10-29 18:40:51 +0900

    Title: 배열 채우기, Time: 98ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241029/배열 채우기/filling-array.java
@@ -0,0 +1,27 @@
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        int arr[] = new int[10];
+        int index = 0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] == 0) {
+                index += i;
+                break;
+            }
+        }
+
+        for (int i = index-1; i >= 0 ; i--) {
+            System.out.printf("%d ", arr[i]);
+        }
+
+        br.close();
+    }
+}
