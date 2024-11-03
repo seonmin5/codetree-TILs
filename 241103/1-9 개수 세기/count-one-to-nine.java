7483dd7ffad2ce156cf38bf23fb4cb458277af75
seonmin5 <82851869+seonmin5@users.noreply.github.com>
2024-11-03 10:36:44 +0900

    Title: 1-9 개수 세기, Time: 98ms, Memory: 8MB, Status: Passed - CodeTree

--- 241103/1-9 개수 세기/count-one-to-nine.java
+++ 241103/1-9 개수 세기/count-one-to-nine.java
@@ -4,23 +4,23 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine().trim());
-        int arr[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int arr2[] = new int[10];
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int[] arr2 = new int[arr.length];
 
         StringTokenizer st = new StringTokenizer(br.readLine().trim());
         for (int i = 0; i < n; i++) {
-            n2 = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < 10; i++) {
+            int n2 = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < arr.length; j++) {
                 if (n2 == arr[j]) {
                     arr2[j]++;
                 }
             }
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < arr2.length; i++) {
             System.out.println(arr2[i]);
         }
 
